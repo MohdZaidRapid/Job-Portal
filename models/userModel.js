e73b0f9943d5bb5a7c321915eaf7d3989cec3836
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-05-11 23:20:54 +0530

    added

--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,32 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 // schema
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: [true, "Name is required"],
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Name is required"],
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      require: [true, "Email is required"],
+      unique: true,
+      validate: validator.isEmail,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    location: {
+      type: String,
+      default: "India",
+    },
   },
-  email: {
-    type: String,
-    require: [true, "Email is required"],
-    unique: true,
-  },
-  
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("User", userSchema);
