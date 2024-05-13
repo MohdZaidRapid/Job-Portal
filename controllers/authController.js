7ea8ee037b269ed68a5fbc3a49d668599afe46dd
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-05-13 18:47:44 +0530

    added

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,44 @@ export const registerController = async (req, res, next) => {
     next("Email Already Registered Please Login");
   }
   const user = await userModel.create({ name, email, password });
-  res
-    .status(201)
-    .send({ success: true, message: "User created successfully", user });
+  // token
+  const token = user.createJWT();
+  res.status(201).send({
+    success: true,
+    message: "User created successfully",
+    user: {
+      name: user.name,
+      email: user.email,
+      location: user.location,
+    },
+    token,
+  });
+};
+
+export const loginController = async (req, res, next) => {
+  const { email, password } = req.body;
+  // validation
+  if (!email || !password) {
+    next("Please Provide All Fields");
+  }
+  // find user by email
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!user) {
+    next("Invalid Username or password");
+  }
+
+  // compare password
+  const isMatch = await user.comparePassword(password);
+  user.password = undefined;
+  if (!isMatch) {
+    next("Invalid Username or password");
+  }
+
+  const token = user.createJWT();
+  res.status(200).json({
+    success: true,
+    message: "Login Successfully",
+    user,
+    token,
+  });
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { registerController } from "../controllers/authController.js";
+import {
+  loginController,
+  registerController,
+} from "../controllers/authController.js";
 
 // router object
 const router = express.Router();
 
 // routes
+
+// REGISTERED || POST
 router.post("/register", registerController);
+
+// LOGIN || POST
+router.post("/login", loginController);
 
 export default router;
