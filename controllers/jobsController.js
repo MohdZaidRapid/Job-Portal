09ea568a5fc9f5ae02c6c598d2b4f845f165da55
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-05-14 15:45:55 +0530

    added

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -16,3 +16,27 @@ export const getAllJobsController = async (req, res, next) => {
   const jobs = await jobsModel.find({ createdBy: req.user.userId });
   res.status(200).json({ totalJobs: jobs.length, jobs });
 };
+
+// Update jobs
+export const updateJobController = async (req, res, next) => {
+  const { id } = req.params;
+  const { company, position } = req.body;
+  if (!company || !position) {
+    next("Please provide all fields");
+  }
+  //   find job
+  const job = await jobsModel.findOne({ _id: id });
+  if (!job) {
+    next(`no jobs found with this id ${id}`);
+  }
+
+  if (req.user.userId !== job.createdBy.toString()) {
+    next("you are not authorized to update this job");
+  }
+  const updateJob = await jobsModel.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  //   res
+  res.status(200).json({ updateJob });
+};
--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -3,6 +3,7 @@ import userAuth from "../middlewares/authMiddleware.js";
 import {
   createJobController,
   getAllJobsController,
+  updateJobController,
 } from "../controllers/jobsController.js";
 
 const router = express.Router();
@@ -13,5 +14,9 @@ router.post("/create-job", userAuth, createJobController);
 
 // GET JOBS || GET
 router.get("/get-job", userAuth, getAllJobsController);
+
+// UPDATE JOBS || PUT || PATCH
+router.patch("/update-job/:id",userAuth,updateJobController)
+
 
 export default router;
