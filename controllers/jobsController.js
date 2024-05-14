634a0f2fc1b832e012c12e8d450557d4c91e7690
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-05-14 19:05:31 +0530

    added

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -55,11 +55,21 @@ export const getAllJobsController = async (req, res, next) => {
   if (sort === "z-a") {
     queryResult = queryResult.sort("-position");
   }
+  //   pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = Number(page - 1) * limit;
+
+  queryResult = queryResult.skip(skip).limit(limit);
+  //   jobs count
+
+  const totalJobs = await jobsModel.countDocuments(queryResult);
+  const numOfPage = Math.ceil(totalJobs / limit);
   const jobs = await queryResult;
 
   //   const jobs = await jobsModel.find({ createdBy: req.user.userId });
 
-  res.status(200).json({ totalJobs: jobs.length, jobs });
+  res.status(200).json({ totalJobs, jobs, numOfPage });
 };
 
 // Update jobs
